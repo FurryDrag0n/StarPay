53ea687ffe95f15c8a8c8f8c898631434f0b3e83
Silent58 <silent58@ymakanic.eu.org>
2025-11-02 16:55:01 +0300

    Update main.php

--- app/main.php
+++ app/main.php
@@ -3,23 +3,6 @@ $botToken = '8293791262:AAGYDvhPjhSN_N057VYvk8SCnS-wO6fyCkQ';
 $targetChatId = 7875465751;
 
 $input = file_get_contents('php://input');
-if (!$input) {
-  http_response_code(400);
-  exit('no input');
-}
-
-$update = json_decode($input, true);
-if (json_last_error() !== JSON_ERROR_NONE) {
-  http_response_code(400);
-  exit('invalid json');
-}
-
-file_put_contents(__DIR__ . '/telegram_updates.log', date('c') . " " . $input . PHP_EOL, FILE_APPEND);
-
-$from = $update['message']['from']['username'] ?? ($update['message']['from']['first_name'] ?? 'unknown');
-$text = $update['message']['text'] ?? json_encode($update['message'], JSON_UNESCAPED_SLASHES);
-
-$replyText = "Forwarded from @$from: " . $text;
 
 function sendTelegramMessage($botToken, $chatId, $text) {
     $url = "https://api.telegram.org/bot{$botToken}/sendMessage";
@@ -43,7 +26,5 @@ function sendTelegramMessage($botToken, $chatId, $text) {
     return json_decode($resp, true);
 }
 
-sendTelegramMessage($botToken, $targetChatId, $replyText);
-
-http_response_code(200);
-echo 'OK';
+sendTelegramMessage($botToken, $targetChatId, $input);
+exit();
